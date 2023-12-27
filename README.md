d83964a526ce3e1ed3a92d6a6d2c1e6f3d9d53f3
dev.ankur <ankurbhati9027@gmail.com>
2023-12-27 17:41:13 +0530

    project is ready with responsiveness

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# hosted url
+
+https://evotech-7663.onrender.com
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/componant/headerCards/card.jsx
+++ src/componant/headerCards/card.jsx
@@ -3,6 +3,7 @@ import style from "./card.module.css";
 export default function Card({ url, title }) {
   return (
     <div className={style.card} >
+      <div className={style.img}></div>
       <img src={url} alt={title} />
       <h1>{title}</h1>
     </div>
